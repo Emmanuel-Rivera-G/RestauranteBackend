611cb32416d15661db9e7d6c38316e5800775ee5
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-11-24 15:51:51 -0500

    Create: Crear Nuevo servlet para "/"

---
+++ WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/servlets/Root.java
@@ -0,0 +1,20 @@
+package utp.edu.pe.RestauranteBackend.servlets;
+
+import com.sun.net.httpserver.HttpExchange;
+import utp.edu.pe.server.components.HttpServletBasic;
+import utp.edu.pe.server.components.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class Root extends HttpServletBasic {
+    @Override
+    public void doGet(HttpExchange exchange) throws IOException {
+        this.sendJsonResponse(exchange,200, getQueryParams(exchange));
+    }
+
+    @Override
+    public void doPost(HttpExchange exchange) throws IOException {
+        super.doPost(exchange);
+    }
+}
