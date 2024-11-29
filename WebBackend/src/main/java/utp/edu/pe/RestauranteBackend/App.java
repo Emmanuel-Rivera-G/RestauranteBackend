abaae8efffac915506c047b9718235d060253749
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-11-28 20:18:16 -0500

    Changes: Cambios en pom.xml y App.java

--- WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/App.java
+++ WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/App.java
@@ -10,9 +10,13 @@ import utp.edu.pe.server.config.WebServer;
 
 public class App {
 
+    private static final int DEFAULT_PORT = 3000;
+
+    private static final String contextPath = "/RestauranteBackend";
+
     public static void main(String[] args) {
-        final int port = 3000;
-        final String contextPath = "/RestauranteBackend";
+
+        final int port = getPort(args);
 
         try {
             WebServer server = new WebServer(port);
@@ -23,5 +27,19 @@ public class App {
         } catch (IOException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private static int getPort(String[] args) {
+        if (args.length == 0) return DEFAULT_PORT;
+        int portForArgs;
+        try {
+            String firstArg = args[0];
+            portForArgs = Integer.parseInt(firstArg);
+        } catch (NumberFormatException numbForEx) {
+            portForArgs = DEFAULT_PORT;
+            Logger.getLogger(App.class.getName()).log(Level.INFO, numbForEx.getMessage());
+            Logger.getLogger(App.class.getName()).log(Level.INFO, "Se utilizará el puerto " + DEFAULT_PORT);
+        }
+        return portForArgs;
     }
 }
