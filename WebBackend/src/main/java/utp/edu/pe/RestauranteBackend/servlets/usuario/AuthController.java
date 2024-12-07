4fddf8df950768cdca6c24aa75c5b1cd8b323eb3
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-12-07 17:59:52 -0500

    Update: Cambiando Métodos en UsuarioController

--- WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/servlets/usuario/AuthController.java
+++ WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/servlets/usuario/AuthController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import jakarta.persistence.EntityManager;
-import utp.edu.pe.RestauranteBackend.model.Usuario;
 import utp.edu.pe.RestauranteBackend.service.UsuarioServiceImpl;
 import utp.edu.pe.RestauranteBackend.service.interfaz.UsuarioService;
 import utp.edu.pe.server.components.HttpServletBasic;
@@ -57,32 +56,5 @@ public class AuthController extends HttpServletBasic {
                     ""
             );
         }
-    }
-
-    @Override
-    public void doPost(HttpExchange exchange) throws IOException {
-        Usuario usuario = this.getRequestBodyAsJson(exchange, Usuario.class);
-        if (usuario != null)
-            if (usuario.getNombreUsuario() != null && usuario.getContrasena() != null) {
-                boolean operacionExitosa = usuarioService.saveUsuario(usuario);
-                Map<String, Object> response = new TreeMap<>();
-                response.put("Operación Exitosa", operacionExitosa);
-                response.put("Usuario", usuario);
-                this.sendJsonResponse(exchange, HttpStatusCode.OK.getCode(), response);
-            } else {
-                Map<String, Object> response = new TreeMap<>();
-                if (usuario.getNombreUsuario() == null)
-                    response.put("Error-Usuario", "El usuario no puede ser null");
-                if (usuario.getContrasena() == null)
-                    response.put("Error-Contrasena", "La contrasena no puede ser null");
-                this.sendJsonResponse(exchange, HttpStatusCode.NON_AUTHORITATIVE_INFORMATION.getCode(), response);
-            }
-        else
-            this.sendAnyHtmlFileResponse(
-                    HttpStatusCode.BAD_REQUEST.getCode(),
-                    exchange,
-                    HttpCodeFallBack.ERROR_FALLBACK_400.getFallBack(),
-                    ""
-            );
     }
 }
--- WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/servlets/usuario/UsuarioController.java
+++ WebBackend/src/main/java/utp/edu/pe/RestauranteBackend/servlets/usuario/UsuarioController.java
@@ -1,10 +1,18 @@
 package utp.edu.pe.RestauranteBackend.servlets.usuario;
 
+import com.sun.net.httpserver.HttpExchange;
 import jakarta.persistence.EntityManager;
+import utp.edu.pe.RestauranteBackend.model.Usuario;
 import utp.edu.pe.RestauranteBackend.service.UsuarioServiceImpl;
 import utp.edu.pe.RestauranteBackend.service.interfaz.UsuarioService;
 import utp.edu.pe.server.components.HttpServletBasic;
 import utp.edu.pe.server.components.WebServlet;
+import utp.edu.pe.server.constants.HttpCodeFallBack;
+import utp.edu.pe.server.constants.HttpStatusCode;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static utp.edu.pe.server.config.ServletHandler.getterPath;
 import static utp.edu.pe.server.config.ServletHandler.sourcePathArchive;
@@ -17,5 +25,37 @@ public class UsuarioController extends HttpServletBasic {
     public UsuarioController(EntityManager entityManager) {
         super(entityManager);
         this.usuarioService = new UsuarioServiceImpl(entityManager);
+    }
+
+    @Override
+    public void doGet(HttpExchange exchange) throws IOException {
+        super.doGet(exchange);
+    }
+
+    @Override
+    public void doPost(HttpExchange exchange) throws IOException {
+        Usuario usuario = this.getRequestBodyAsJson(exchange, Usuario.class);
+        if (usuario != null)
+            if (usuario.getNombreUsuario() != null && usuario.getContrasena() != null) {
+                boolean operacionExitosa = usuarioService.saveUsuario(usuario);
+                Map<String, Object> response = new TreeMap<>();
+                response.put("Operación Exitosa", operacionExitosa);
+                response.put("Usuario", usuario);
+                this.sendJsonResponse(exchange, HttpStatusCode.OK.getCode(), response);
+            } else {
+                Map<String, Object> response = new TreeMap<>();
+                if (usuario.getNombreUsuario() == null)
+                    response.put("Error-Usuario", "El usuario no puede ser null");
+                if (usuario.getContrasena() == null)
+                    response.put("Error-Contrasena", "La contrasena no puede ser null");
+                this.sendJsonResponse(exchange, HttpStatusCode.NON_AUTHORITATIVE_INFORMATION.getCode(), response);
+            }
+        else
+            this.sendAnyHtmlFileResponse(
+                    HttpStatusCode.BAD_REQUEST.getCode(),
+                    exchange,
+                    HttpCodeFallBack.ERROR_FALLBACK_400.getFallBack(),
+                    ""
+            );
     }
 }
